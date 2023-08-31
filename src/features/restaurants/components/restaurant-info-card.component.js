734b7a76569e2ab355a13f424ed63fbedd9cb130
Islam Khattab <islam.n.khattab@gmail.com>
2023-08-31 13:00:15 -0400

    Adding resturant initial card

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,8 +1,12 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
 import { styled } from "styled-components/native";
-import { Text } from "react-native";
+import { Text, View, Image } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+
+import open from "../../../../assets/open";
+import star from "../../../../assets/star";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -28,23 +32,60 @@ const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const Section = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+`;
+
+const SectionEnd = styled.View`
+  flex-direction: row;
+  flex: 1;
+  justify-content: flex-end;
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
     isOpenNow = true,
-    rating = 4,
-    isClosedTemporarily,
+    rating = 5,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map((_, index) => {
+              return <SvgXml key={index} width={20} height={20} xml={star} />;
+            })}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <View style={{ paddingLeft: 16 }} />
+            {isOpenNow && <SvgXml width={20} height={20} xml={open} />}
+            <View style={{ paddingLeft: 16 }} />
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
