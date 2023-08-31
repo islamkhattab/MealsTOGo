8b3e27dfabf43952137be9f7e50de35f8225222b
Islam Khattab <islam.n.khattab@gmail.com>
2023-08-31 17:50:19 -0400

    Adding Restuarant List

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,9 +1,13 @@
-/* eslint-disable prettier/prettier */
 import * as React from "react";
 import { styled } from "styled-components/native";
 import { Searchbar } from "react-native-paper";
 import { SafeAreaView, StatusBar } from "react-native";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import {
+  Spacer,
+  spacerPostitions,
+  spacerSizes,
+} from "../../../components/spacer/spacer.component";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
@@ -14,10 +18,11 @@ const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-`;
+const RestaurantList = styled.FlatList.attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
 export const RestuarantScreen = () => {
   return (
@@ -25,9 +30,37 @@ export const RestuarantScreen = () => {
       <SearchContainer>
         <Searchbar placeholder="Search..." />
       </SearchContainer>
-      <RestaurantListContainer>
-        <RestaurantInfoCard />
-      </RestaurantListContainer>
+      <RestaurantList
+        data={[
+          { id: 1 },
+          { id: 2 },
+          { id: 3 },
+          { id: 4 },
+          { id: 5 },
+          { id: 6 },
+          { id: 7 },
+          { id: 8 },
+          { id: 9 },
+          { id: 10 },
+          { id: 11 },
+          { id: 12 },
+          { id: 13 },
+          { id: 14 },
+          { id: 15 },
+          { id: 16 },
+          { id: 17 },
+          { id: 18 },
+          { id: 19 },
+          { id: 20 },
+          { id: 21 },
+        ]}
+        renderItem={() => (
+          <Spacer position={spacerPostitions.bottom} size={spacerSizes.large}>
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
+        keyExtractor={(item) => item.id}
+      />
     </SafeArea>
   );
 };
