2d0ef3369fa83722d546fdbda4d4e2bcae3a66eb
Islam Khattab <islam.n.khattab@gmail.com>
2023-09-01 15:17:47 -0400

    Adding mock service

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { styled } from "styled-components/native";
 import { Searchbar } from "react-native-paper";
-import { SafeAreaView, StatusBar } from "react-native";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import {
   Spacer,
